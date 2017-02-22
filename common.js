b8a1770b7d4db25267e6291839e155b050919014
chalyi-andrei <andrey.chaluy@gmail.com>
2017-02-22 09:38:18 +0200

    start

---
+++ common.js
@@ -0,0 +1,17 @@
+
+// //load content
+// $('.load-btn').on('click', function() {
+//     $('.content').load('content.html', function() {
+//         console.log('Загрузили контент на сайт');
+//     });
+// });
+// //DEFERED WHEN()
+// $('.hide-btn').on('click', function() {
+//     $.when(
+//         $('.main3>img').slideUp(1000),
+//         $('.main3>p').slideUp(3000)
+//     ).then(function() {
+//         $('.main3').text('Цепочка событий: Картинки+параграф-> контент');
+//     }
+//     );
+// });
