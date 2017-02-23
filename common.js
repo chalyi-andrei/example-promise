c4c01b5ced2febbb283f3298e151866ee3cf6237
chalyi-andrei <andrey.chaluy@gmail.com>
2017-02-23 19:10:42 +0200

    added simple example promise

--- common.js
+++ common.js
@@ -20,13 +20,32 @@ $('.btn1').on('click', function() {
     $('.main2').load('user.json');
 });
 $('.btn2').on('click', function() {
-
-
     var user = 'https://jsonplaceholder.typicode.com/photos/1';
     $.getJSON(user, function(response) {
         console.log(response);
         $('.img').attr('src',response.thumbnailUrl);
     });
-
-
 });
+//PROMISE
+$('.btn3').on('click', function() {
+    let promise = new Promise(function(resolve, reject){
+        setTimeout(() => {
+            resolve();
+        },1500);
+    });
+    promise
+        .then(() => {
+            let x = 10;
+            var user = 'https://jsonplaceholder.typicode.com/ph2otos/3';
+            return $.getJSON(user, function(response) {
+                $('.img').attr('src',response.thumbnailUrl);
+            });
+        })
+        .then((photo) => {
+            var userPhoto = photo;
+            $('.paragraph').after('<p>Цепочка промисов закончена url фотографии= ' + photo + '</p>');
+        })
+        .catch((reject) => alert('Error! ' + reject));
+        $('.paragraph').text('Сначала выполнится эта строчка, а промис выполнится асинхронно');
+});
+
